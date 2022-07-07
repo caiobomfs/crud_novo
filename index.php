92d63e81300c37387371323a5888f828a4ad9f60
caiobomfs <caiobomfimfc@gmail.com>
2022-07-07 15:29:16 -0300

    first commit

--- index.php
+++ index.php
@@ -1,38 +1,47 @@
 <?php 
+/**
+ * Main
+ * Pagina principal do site
+ * php version  8.1.4
+ * 
+ * @category Text/html
+ * @package  Apache
+ * @author   Caio Bomfim <caiobomfimfc@gmail.com>
+ * @license  https://github.com/caiobomfs/testecrud 
+ * @link     https://github.com/caiobomfs/testecrud 
+ */
   require './config/config.php';
-
   $post = filter_input_array(INPUT_POST, FILTER_DEFAULT);
   $get = filter_input_array(INPUT_GET, FILTER_DEFAULT);
   
  
   
-  if(isset($post['contatos']) && $post['contatos'] == 1){
-    
-      if(!empty($post['nome'])){
+if (isset($post['contatos']) && $post['contatos'] == 1) {
+    if (!empty($post['nome']) ) {
 
-          $nome = $post['nome'];
-          $email = $post['email'];
-          $numero = $post['numero'];
-          $sql = "INSERT INTO `contatos` (`id`, `nome`, `email`, `numero`) VALUES (NULL, '$nome', '$email', '$numero');";
-          
-          $db->query($sql);
-      }
+        $nome = $post['nome'];
+        $email = $post['email'];
+        $numero = $post['numero'];
+        $sql = "INSERT INTO `contatos` (`id`, `nome`, `email`, `numero`) VALUES (NULL, '$nome', '$email', '$numero');";
+        
+        $db->query($sql);
+    }
 
-  }
+}
 
 
  
 
 
-  if(isset($get['edit']) && $get['edit'] == 1){
+if (isset($get['edit']) && $get['edit'] == 1) {
   
       $id = $get['id'];
       $select_edit = $db->query("SELECT * FROM contatos WHERE id = '$id'");
       $list = $select_edit->fetch_assoc();
 
-  }
+}
 
-  if(isset($post['edit']) && $post['edit'] == 1) {
+if (isset($post['edit']) && $post['edit'] == 1) {
 
       $id = $post['id'];
       $nome = $post['nome'];
@@ -44,16 +53,17 @@
 
       $db->query($sql_update);
 
-  }
+}
 
 
-  if(isset($get['dell']) && $get['dell'] == 1){
+if (isset($get['dell']) && $get['dell'] == 1) {
+
       $id = $get['id'];
 
       $sql_dell = "DELETE FROM contatos WHERE  id = '$id'";
 
-      $db->query($sql_dell);
-  }
+      $db->query($sql_dell); 
+}
 
    $select = $db->query("SELECT * FROM contatos");
   
@@ -61,92 +71,109 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.slim.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Description" content="Enter your description here"/>
+  <link rel="stylesheet" href="./CSS/main.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.slim.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
-<title>Agenda</title>
+  <title>Agenda</title>
 </head>
 <body class="bg-info">
-    <header>
-        <h1 class="d-flex justify-content-center mb-5 text-white">Bem vindo a agenda Telefonica</h1>
-    </header>
+  <header>
+    <h1 class="d-flex justify-content-center mb-5 text-white">
+      Bem vindo a agenda Telefonica</h1>
+  </header>
 
         
       
   
-        <section class="  rounded mx-3   ">
-        <table class="table table-bordered   table-responsive border-dark bg-light">
-            <thead>
-              <tr style="background-color: #0CC0AB ; color: white;  font-weight: 800; font-size: 19px; ">
-                <th scope="col-lg-2" class="col-lg-1">#id</th>
-                <th scope="col-lg-2" class="col-lg-4">Nome</th>
-                <th scope="col-lg-2" class="col-lg-4">E-mail</th>
-                <th scope="col-lg-2" class="col-lg-2">Telefone</th>
-                <th class="col-lg-1"  style="height:50px; width:50px;"  style="width: 12.499999995%;flex: 0 0 12.499%;max-width: 12.499%;">
-                <img class="img-fluid" src="./images/lapis.jpg" style="height:50px; width:50px;"></th>
-                <th class="col-lg-1" style="height:50px; width:50px;"  style="width: 12.499999995%;flex: 0 0 12.499%;max-width: 12.499%;">
-                <img class="img-fluid" src="./images/lixeira.jpg" style="height:50px; width:50px;"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-                while($linhas = $select->fetch_assoc()){
+<section class="  rounded mx-3   ">
+  <table class="table table-bordered   table-responsive border-dark bg-light">
+    <thead>
+      <tr class="bg">
+        <th scope="col-lg-2" class="col-lg-1">#id</th>
+        <th scope="col-lg-2" class="col-lg-4">Nome</th>
+        <th scope="col-lg-2" class="col-lg-4">E-mail</th>
+        <th scope="col-lg-2" class="col-lg-2">Telefone</th>
+        <th class="col-lg-1 header_imagem"  >
+        <img class="img-fluid img_botao" src="./images/lapis.jpg"></th>
+        <th class="col-lg-1 header_imagem" >
+        <img class="img-fluid img_botao" src="./images/lixo.jpg"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        while ($linhas = $select->fetch_assoc()) {
 
-              ?>
-              <tr  style=" color:  black;  font-weight: 700; font-size: 16px; ">
-                  <td ><?=$linhas['id'] ?></td>
-                  <td ><?=$linhas['nome'] ?></td>
-                  <td ><?=$linhas['email'] ?></td>
-                  <td ><?=$linhas['numero'] ?></td>
-                  
-             
+            ?>
+          <tr class="fonte">
+              <td ><?php echo$linhas['id'] ?></td>
+              <td ><?php echo$linhas['nome'] ?></td>
+              <td ><?php echo$linhas['email'] ?></td>
+              <td ><?php echo$linhas['numero'] ?></td>
+          
               <td>
-                     <a id="anc1" href="index.php?edit=1&id=<?= $linhas['id']?>" >
-                        <button class="btn text-nowrap" href="index.php?edit=1&id=<?= $linhas['id']?>" type="button"data-bs-toggle="tooltip" data-bs-placement="right" title="editar esse contato" style="height:50px; width:50px; padding:0px 0px ;">
-                          <img class="img-fluid" src="./images/engrenagem.jpg" style="height:50px; width:50px;">
-                        </button>
-                      </a>  
-              </td>
-                    <td >
-                     
-                        <button class="btn text-nowrap" type="button" data-toggle="modal" data-target="#modalAviso" data-bs-toggle="tooltip" data-bs-placement="right" title="excluir esse contato" style="height:50px; width:50px; padding:0px 0px ;">
-                          <img class="img-fluid" src="./images/deletar.jpg" style="height:50px; width:50px;">
-                        </button>
-                      
-                    </td>
-                    </tr>
-                    <div class="modal fade" id="modalAviso" tabindex="-1" role="dialog" aria-labelledby="modalAvisoLabel" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header ">
-                        <h5 class="modal-title" id="modalAvisoLabel">Aviso</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body d-flex justify-content-center">
-                        <p class="" style="font-size: 20px; font-weight:600;">Tem certeza que deseja excluir esse contato?</p>
-                      </div>
-                      <div class="modal-footer d-flex justify-content-center">
-                        <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Não</button>
+                <a id="anc1" 
+                  href="index.php?edit=1&id=<?php echo $linhas['id']?>" >
 
-                        <a id="anc2" href="index.php?dell=1&id=<?= $linhas['id']?>" >
-                        <button class="btn btn-primary btn-lg" type="button" >Sim</button>
-                        </a> 
-                      </div>
-                    </div>
+                  <button class="btn text-nowrap img_botao padder" 
+                  href="index.php?edit=1&id=<?php echo $linhas['id']?>" 
+                  type="button"data-bs-toggle="tooltip" 
+                  data-bs-placement="right" title="editar esse contato" >
+                    <img class="img-fluid img_botao" src="./images/cog.jpg">
+                  </button>
+                </a>  
+              </td>
+              <td >
+                <button class="btn text-nowrap img_botao padder" 
+                  type="button" data-toggle="modal" 
+                  data-target="#modalAviso" data-bs-toggle="tooltip" 
+                  data-bs-placement="right" title="excluir esse contato" >
+                  <img class="img-fluid img_botao" src="./images/del.jpg">
+                </button>
+              </td>
+            </tr>
+            <div class="modal fade" id="modalAviso" tabindex="-1" 
+                role="dialog" aria-labelledby="modalAvisoLabel" 
+                aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" 
+                role="document">
+
+                <div class="modal-content">
+                  <div class="modal-header ">
+                    <h5 class="modal-title" id="modalAvisoLabel">Aviso</h5>
+                    <button type="button" class="close" data-dismiss="modal" 
+                      aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body d-flex justify-content-center">
+                    <p class="font_modal">
+                      Tem certeza que deseja excluir esse contato?
+                    </p>
+                  </div>
+                  <div class="modal-footer d-flex justify-content-center">
+                    <button type="button" class="btn btn-secondary btn-lg" 
+                    data-dismiss="modal">Não</button>
+
+                    <a id="anc2" 
+                      href="index.php?dell=1&id=<?php echo $linhas['id']?>" >
+                      <button class="btn btn-primary btn-lg" 
+                      type="button" >Sim</button>
+                    </a> 
                   </div>
                 </div>
-              <?php }?>
-            </tbody>  
-          </table>
-        </section>
+              </div>
+            </div>
+        <?php }?>
+    </tbody>  
+    </table>
+</section>
       
        
 
@@ -163,27 +190,27 @@
 
     <div class="d-flex justify-content-center">
                 
-                <?php if(isset($get['edit']) && $get['edit'] == 1){ ?>
+                <?php if (isset($get['edit']) && $get['edit'] == 1) { ?>
                   
                   <form action="index.php" method="post" id="form1">
                   <div id="ed" >
                     <input type="hidden" name="edit" id="edit" value="1">
-                    <input type="hidden" name="id" id="idE" value="<?= $list['id'] ?>">
+                    <input type="hidden" name="id" id="idE" value="<?php echo $list['id'] ?>">
                     <label for="nome">Nome</label><br>
-                    <input type="text" name="nome" id="nomeE" value="<?= $list['nome'] ?>">
+                    <input type="text" name="nome" id="nomeE" value="<?php echo $list['nome'] ?>">
                     <br>
                     <label for="email">email</label><br>
-                    <input type="text" name="email" id="emailE" value="<?=  $list['email'] ?>">
+                    <input type="text" name="email" id="emailE" value="<?php echo  $list['email'] ?>">
                     <br>
                     <label for="numero">Telefone</label><br>
-                    <input type="text" name="numero" id="numeroE" value="<?= $list['numero'] ?>">
+                    <input type="text" name="numero" id="numeroE" value="<?php echo $list['numero'] ?>">
                     <br>
                     <br>
                     <input type="submit" value="gravar edições " id="editSave" form="form1"> 
                 
                   </div>
             </form>  
-                  <?php }else {?>
+                <?php } else {?>
 
                   
             <form action="index.php" method="post" id="form2">
@@ -205,7 +232,7 @@
                 <br>
                 </div>
           </form>
-          <?php }?>
+                <?php }?>
         </div>
         
         
